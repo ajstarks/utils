482bf0544897f28c4a63b547e2ae667c697c36c5
Anthony Starks <ajstarks@gmail.com>
2022-03-03 16:08:48 -0500

    distable: read data from a file

---
+++ cmd/distable/morris.d
@@ -0,0 +1,561 @@
+Beavertown
+Boonton
+	Beavertown:5.30
+Budds Lake
+	Beavertown:21.30
+	Boonton:16.00
+Chatham
+	Beavertown:12.20
+	Boonton:10.40
+	Budds Lake:18.70
+Chester
+	Beavertown:21.40
+	Boonton:16.30
+	Budds Lake:5.40
+	Chatham:15.10
+Denville
+	Beavertown:8.60
+	Boonton:3.30
+	Budds Lake:12.70
+	Chatham:10.50
+	Chester:13.00
+Dover
+	Beavertown:12.80
+	Boonton:7.50
+	Budds Lake:8.50
+	Chatham:12.40
+	Chester:9.30
+	Denville:4.10
+Drakestown
+	Beavertown:23.80
+	Boonton:18.50
+	Budds Lake:2.60
+	Chatham:20.50
+	Chester:5.50
+	Denville:15.20
+	Dover:11.10
+Flanders
+	Beavertown:19.70
+	Boonton:14.40
+	Budds Lake:2.20
+	Chatham:16.50
+	Chester:3.90
+	Denville:11.10
+	Dover:7.00
+	Drakestown:4.20
+German Valley
+	Beavertown:2.50
+	Boonton:19.20
+	Budds Lake:5.60
+	Chatham:19.80
+	Chester:1.10
+	Denville:16.60
+	Dover:12.60
+	Drakestown:3.70
+	Flanders:5.90
+Green Village
+	Beavertown:13.80
+	Boonton:10.80
+	Budds Lake:15.30
+	Chatham:3.30
+	Chester:12.50
+	Denville:9.80
+	Dover:10.50
+	Drakestown:11.40
+	Flanders:13.60
+	German Valley:16.60
+Hackettstown
+	Beavertown:26.10
+	Boonton:20.90
+	Budds Lake:4.90
+	Chatham:22.80
+	Chester:7.40
+	Denville:17.60
+	Dover:13.40
+	Drakestown:2.40
+	Flanders:6.70
+	German Valley:4.20
+	Green Village:19.60
+Hanover
+	Beavertown:8.30
+	Boonton:6.10
+	Budds Lake:18.20
+	Chatham:4.00
+	Chester:16.20
+	Denville:7.60
+	Dover:10.50
+	Drakestown:20.30
+	Flanders:15.00
+	German Valley:20.40
+	Green Village:5.80
+	Hackettstown:22.70
+Hibernia
+	Beavertown:9.10
+	Boonton:4.70
+	Budds Lake:13.10
+	Chatham:14.00
+	Chester:14.80
+	Denville:3.60
+	Dover:5.50
+	Drakestown:15.70
+	Flanders:12.00
+	German Valley:17.80
+	Green Village:13.40
+	Hackettstown:18.20
+	Hanover:10.90
+Hurdtown
+	Beavertown:14.00
+	Boonton:9.40
+	Budds Lake:9.90
+	Chatham:17.40
+	Chester:13.20
+	Denville:7.20
+	Dover:5.60
+	Drakestown:12.50
+	Flanders:9.60
+	German Valley:15.30
+	Green Village:16.00
+	Hackettstown:14.40
+	Hanover:14.90
+	Hiberbia:4.90
+Madison
+	Beavertown:11.90
+	Boonton:9.40
+	Budds Lake:16.80
+	Chatham:1.80
+	Chester:14.00
+	Denville:9.00
+	Dover:10.50
+	Drakestown:18.60
+	Flanders:14.70
+	German Valley:18.10
+	Green Village:2.00
+	Hackettstown:20.90
+	Hanover:3.70
+	Hiberbia:12.70
+	Hurdtown:15.70
+Mendham
+	Beavertown:17.80
+	Boonton:12.90
+	Budds Lake:8.40
+	Chatham:11.10
+	Chester:4.50
+	Denville:10.10
+	Dover:7.50
+	Drakestown:9.60
+	Flanders:6.30
+	German Valley:8.70
+	Green Village:7.90
+	Hackettstown:11.80
+	Hanover:11.90
+	Hiberbia:12.70
+	Hurdtown:12.60
+	Madison:9.40
+Milton
+	Beavertown:11.90
+	Boonton:8.90
+	Budds Lake:14.60
+	Chatham:18.70
+	Chester:17.60
+	Denville:8.30
+	Dover:9.00
+	Drakestown:17.10
+	Flanders:14.20
+	German Valley:19.90
+	Green Village:18.10
+	Hackettstown:19.10
+	Hanover:15.40
+	Hibernia:4.60
+	Hurdtown:4.70
+	Madison:17.30
+	Mendham:16.40
+Montville
+	Beavertown:3.20
+	Boonton:2.20
+	Budds Lake:18.10
+	Chatham:11.50
+	Chester:18.40
+	Denville:5.50
+	Dover:9.60
+	Drakestown:20.70
+	Flanders:16.60
+	German Valley:22.10
+	Green Village:12.40
+	Hackettstown:23.00
+	Hanover:7.60
+	Hibernia:6.00
+	Hurdtown:10.90
+	Madison:10.80
+	Mendham:15.10
+	Milton:9.30
+Morristown
+	Beavertown:11.70
+	Boonton:7.60
+	Budds Lake:13.10
+	Chatham:5.80
+	Chester:10.30
+	Denville:6.00
+	Dover:6.60
+	Drakestown:15.00
+	Flanders:10.90
+	German Valley:15.00
+	Green Village:4.00
+	Hackettstown:17.40
+	Hanover:5.40
+	Hibernia:9.60
+	Hurdtown:12.00
+	Madison:4.00
+	Mendham:6.50
+	Milton:14.20
+	Montville:9.50
+New Vernon
+	Beavertown:14.80
+	Boonton:11.00
+	Budds Lake:13.60
+	Chatham:5.70
+	Chester:10.10
+	Denville:9.30
+	Dover:9.10
+	Drakestown:15.10
+	Flanders:11.40
+	German Valley:14.20
+	Green Village:2.40
+	Hackettstown:17.30
+	Hanover:7.40
+	Hibernia:12.80
+	Hurdtown:14.70
+	Madison:4.10
+	Mendham:5.50
+	Milton:17.30
+	Montville:12.90
+	Morristown:3.40
+Parsippany
+	Beavertown:9.60
+	Boonton:2.50
+	Budds Lake:15.50
+	Chatham:8.00
+	Chester:15.00
+	Denville:3.30
+	Dover:7.10
+	Drakestown:17.90
+	Flanders:13.70
+	German Valley:18.80
+	Green Village:4.30
+	Hackettstown:20.40
+	Hanover:4.60
+	Hibernia:6.30
+	Hurdtown:10.50
+	Madison:7.00
+	Mendham:11.25
+	Milton:10.80
+	Montville:4.20
+	Morristown:5.40
+	New Vernon:8.70
+Pompton Plains
+	Beavertown:3.40
+	Boonton:7.40
+	Budds Lake:22.70
+	Chatham:15.50
+	Chester:23.50
+	Denville:10.40
+	Dover:14.40
+	Drakestown:25.30
+	Flanders:21.30
+	German Valley:26.90
+	Green Village:17.00
+	Hackettstown:27.60
+	Hanover:11.60
+	Hibernia:9.70
+	Hurdtown:14.20
+	Madison:15.30
+	Mendham:20.30
+	Milton:11.40
+	Montville:5.10
+	Morristown:14.60
+	New Vernon:17.80
+	Parsippany:9.10
+Port Oram
+	Beavertown:13.60
+	Boonton:8.40
+	Budds Lake:7.70
+	Chatham:13.60
+	Chester:9.20
+	Denville:5.20
+	Dover:1.30
+	Drakestown:10.20
+	Flanders:6.40
+	German Valley:12.10
+	Green Village:11.70
+	Hackettstown:12.60
+	Hanover:11.80
+	Hibernia:5.70
+	Hurdtown:4.80
+	Madison:11.90
+	Mendham:7.90
+	Milton:8.60
+	Montville:10.50
+	Morristown:7.90
+	New Vernon:10.20
+	Parsippany:8.30
+	Pompton Plains:15.00
+Rockaway
+	Beavertown:10.30
+	Boonton:5.00
+	Budds Lake:11.00
+	Chatham:11.80
+	Chester:11.80
+	Denville:1.90
+	Dover:2.50
+	Drakestown:13.50
+	Flanders:9.40
+	German Valley:15.10
+	Green Village:10.60
+	Hackettstown:15.90
+	Hanover:9.30
+	Hibernia:3.40
+	Hurdtown:5.60
+	Madison:10.20
+	Mendham:9.30
+	Milton:7.70
+	Montville:7.20
+	Morristown:6.60
+	New Vernon:9.70
+	Parsippany:5.20
+	Pompton Plains:11.90
+	Port Oram:3.30
+Schooleys Mountain
+	Beavertown:26.30
+	Boonton:12.00
+	Budds Lake:5.80
+	Chatham:21.60
+	Chester:5.90
+	Denville:17.80
+	Dover:13.70
+	Drakestown:3.30
+	Flanders:6.70
+	German Valley:1.90
+	Green Village:18.40
+	Hackettstown:2.70
+	Hanover:22.00
+	Hibernia:18.70
+	Hurdtown:15.70
+	Madison:19.90
+	Mendham:10.50
+	Milton:20.40
+	Montville:23.30
+	Morristown:16.60
+	New Vernon:16.00
+	Parsippany:20.20
+	Pompton Plains:28.00
+	Port Oram:13.00
+	Rockaway:16.20
+Shippenport
+	Beavertown:17.40
+	Boonton:21.10
+	Budds Lake:4.20
+	Chatham:16.60
+	Chester:7.60
+	Denville:8.90
+	Dover:4.90
+	Drakestown:6.80
+	Flanders:3.70
+	German Valley:9.40
+	Green Village:14.30
+	Hackettstown:9.10
+	Hanover:15.30
+	Hibernia:8.90
+	Hurdtown:5.80
+	Madison:14.80
+	Mendham:8.30
+	Milton:10.50
+	Montville:14.20
+	Morristown:10.80
+	New Vernon:12.40
+	Parsippany:12.00
+	Pompton Plains:18.60
+	Port Oram:3.70
+	Rockaway:7.10
+	Schooleys Mountain:10.00
+Stanhope
+	Beavertown:19.90
+	Boonton:14.70
+	Budds Lake:2.60
+	Chatham:18.80
+	Chester:7.50
+	Denville:11.50
+	Dover:7.40
+	Drakestown:4.90
+	Flanders:3.60
+	German Valley:8.20
+	Green Village:16.30
+	Hackettstown:6.90
+	Hanover:17.70
+	Hibernia:11.30
+	Hurdtown:7.50
+	Madison:17.00
+	Mendham:9.50
+	Milton:12.20
+	Montville:16.70
+	Morristown:13.00
+	New Vernon:14.20
+	Parsippany:14.60
+	Pompton Plains:21.00
+	Port Oram:6.30
+	Rockaway:9.60
+	Schooleys Mountain:8.20
+	Shippenport:2.50
+Stephensburgh
+	Beavertown:29.40
+	Boonton:24.20
+	Budds Lake:8.50
+	Chatham:24.80
+	Chester:9.10
+	Denville:20.50
+	Dover:16.80
+	Drakestown:5.90
+	Flanders:9.80
+	German Valley:5.00
+	Green Village:21.60
+	Hackettstown:3.90
+	Hanover:25.20
+	Hibernia:21.60
+	Hurdtown:18.20
+	Madison:23.10
+	Mendham:13.60
+	Milton:22.90
+	Montville:26.40
+	Morristown:19.90
+	New Vernon:19.20
+	Parsippany:23.40
+	Pompton Plains:31.10
+	Port Oram:16.10
+	Rockaway:19.20
+	Schooleys Mountain:3.20
+	Shippenport:12.80
+	Stanhope:10.80
+Succasunny
+	Beavertown:16.50
+	Boonton:11.20
+	Budds Lake:4.90
+	Chatham:14.50
+	Chester:6.20
+	Denville:11.95
+	Dover:3.80
+	Drakestown:7.30
+	Flanders:3.20
+	German Valley:8.90
+	Green Village:11.90
+	Hackettstown:9.70
+	Hanover:13.40
+	Hibernia:8.90
+	Hurdtown:7.10
+	Madison:12.60
+	Mendham:6.00
+	Milton:11.50
+	Montville:13.40
+	Morristown:8.70
+	New Vernon:9.90
+	Parsippany:10.70
+	Pompton Plains:18.20
+	Port Oram:3.20
+	Rockaway:6.30
+	Schooleys Mountain:9.90
+	Shippenport:2.40
+	Stanhope:4.40
+	Stephensburgh:13.00
+Walnut Grove
+	Beavertown:14.10
+	Boonton:9.50
+	Budds Lake:8.10
+	Chatham:10.70
+	Chester:6.70
+	Denville:9.50
+	Dover:3.70
+	Drakestown:10.70
+	Flanders:5.90
+	German Valley:10.60
+	Green Village:8.10
+	Hackettstown:12.50
+	Hanover:10.10
+	Hibernia:8.90
+	Hurdtown:9.10
+	Madison:8.90
+	Mendham:3.80
+	Milton:12.70
+	Montville:11.80
+	Morristown:5.00
+	New Vernon:6.30
+	Parsippany:8.60
+	Pompton Plains:16.80
+	Port Oram:4.40
+	Rockaway:5.50
+	Schooleys Mountain:12.00
+	Shippenport:6.20
+	Stanhope:8.10
+	Stephensburgh:15.70
+	Succasunny:3.80
+Waterville
+	Beavertown:15.10
+	Boonton:10.20
+	Budds Lake:9.30
+	Chatham:9.40
+	Chester:6.80
+	Denville:7.50
+	Dover:5.50
+	Drakestown:11.00
+	Flanders:7.10
+	German Valley:10.90
+	Green Village:6.60
+	Hackettstown:13.40
+	Hanover:9.50
+	Hibernia:10.40
+	Hurdtown:11.10
+	Madison:7.60
+	Mendham:2.70
+	Milton:14.40
+	Montville:12.40
+	Morristown:4.10
+	New Vernon:4.40
+	Parsippany:8.60
+	Pompton Plains:17.60
+	Port Oram:6.30
+	Rockaway:7.00
+	Schooleys Mountain:12.50
+	Shippenport:7.90
+	Stanhope:9.70
+	Stephensburgh:15.70
+	Succasunny:5.50
+	Walnut Grove:7.90
+Whippany
+	Beavertown:8.40
+	Boonton:5.30
+	Budds Lake:16.00
+	Chatham:5.20
+	Chester:14.40
+	Denville:5.50
+	Dover:8.20
+	Drakestown:18.10
+	Flanders:13.90
+	German Valley:18.40
+	Green Village:5.60
+	Hackettstown:20.50
+	Hanover:2.30
+	Hibernia:8.90
+	Hurdtown:12.10
+	Madison:4.10
+	Mendham:10.10
+	Milton:13.50
+	Montville:6.70
+	Morristown:3.60
+	New Vernon:6.40
+	Parsippany:2.90
+	Pompton Plains:11.50
+	Port Oram:9.50
+	Rockaway:7.00
+	Schooleys Mountain:20.00
+	Shippenport:13.00
+	Stanhope:15.40
+	Stephensburgh:23.20
+	Succasunny:11.20
+	Walnut Grove:8.00
+	Waterville:7.60
