b9e38d78569168708f3de975d22f2cda6d89055f
Anthony Starks <ajstarks@gmail.com>
2022-03-06 07:57:52 -0500

    distable: add African cities, script to build all tables

---
+++ cmd/distable/af-km.d
@@ -0,0 +1,528 @@
+Abidjan
+Accra
+	Abidjan:422.23
+Addis Ababa
+	Abidjan:4725.57
+	Accra:4304.58
+Alexandria
+	Abidjan:4555.58
+	Accra:4239.02
+	Addis Ababa:2624.86
+Algiers
+	Abidjan:3564.98
+	Accra:3484.11
+	Addis Ababa:4726.08
+	Alexandria:2542.49
+Antananarivo
+	Abidjan:6250.09
+	Accra:5888.23
+	Addis Ababa:3252.42
+	Alexandria:5876.54
+	Algiers:7745.47
+Bamako
+	Abidjan:921.57
+	Accra:1162.15
+	Addis Ababa:5105.98
+	Alexandria:4386.75
+	Algiers:2896.90
+	Antananarivo:7017.34
+Brazzaville
+	Abidjan:2394.10
+	Accra:2036.44
+	Addis Ababa:2984.67
+	Alexandria:4234.08
+	Algiers:4731.46
+	Antananarivo:3856.42
+	Bamako:3184.09
+Cairo
+	Abidjan:4599.52
+	Accra:4271.16
+	Addis Ababa:2460.18
+	Alexandria:180.18
+	Algiers:2708.81
+	Antananarivo:5712.60
+	Bamako:4467.77
+	Brazzaville:4173.48
+Cape Town
+	Abidjan:4958.51
+	Accra:4802.64
+	Addis Ababa:5229.96
+	Alexandria:7334.88
+	Algiers:8013.18
+	Antananarivo:3323.14
+	Bamako:5879.92
+	Brazzaville:3311.25
+	Cairo:7234.15
+Casablanca
+	Abidjan:3160.59
+	Accra:3207.76
+	Addis Ababa:5452.36
+	Alexandria:3512.01
+	Algiers:1029.69
+	Antananarivo:8251.79
+	Bamako:2327.07
+	Brazzaville:4844.46
+	Cairo:3668.66
+	Cape Town:7977.09
+Conakry
+	Abidjan:1162.96
+	Accra:1549.99
+	Addis Ababa:5742.24
+	Alexandria:5097.71
+	Algiers:3466.19
+	Antananarivo:7411.64
+	Bamako:713.90
+	Brazzaville:3556.54
+	Cairo:5180.64
+	Cape Town:5896.18
+	Casablanca:2747.46
+Dakar
+	Abidjan:1795.10
+	Accra:2137.02
+	Addis Ababa:6122.71
+	Alexandria:5136.90
+	Algiers:3183.57
+	Antananarivo:8025.06
+	Bamako:1043.36
+	Brazzaville:4172.47
+	Cairo:5243.06
+	Cape Town:6597.92
+	Casablanca:2323.26
+	Conakry:701.92
+Dar es Salaam
+	Abidjan:4988.87
+	Accra:4590.10
+	Addis Ababa:1760.50
+	Alexandria:4337.84
+	Algiers:6132.94
+	Antananarivo:1613.88
+	Bamako:5650.44
+	Brazzaville:2668.75
+	Cairo:4183.28
+	Cape Town:3697.47
+	Casablanca:6668.79
+	Conakry:6140.06
+	Dakar:6688.17
+Douala
+	Abidjan:1526.96
+	Accra:1110.55
+	Addis Ababa:3247.38
+	Alexandria:3683.31
+	Algiers:3697.71
+	Antananarivo:4859.58
+	Bamako:2165.94
+	Brazzaville:1112.15
+	Cairo:3669.47
+	Cape Town:4316.66
+	Casablanca:3739.67
+	Conakry:2652.26
+	Dakar:3195.76
+	Dar es Salaam:3495.70
+Durban
+	Abidjan:5405.58
+	Accra:5152.29
+	Addis Ababa:4397.91
+	Alexandria:6784.31
+	Algiers:7954.79
+	Antananarivo:2060.89
+	Bamako:6303.97
+	Brazzaville:3288.90
+	Cairo:6655.80
+	Cape Town:1270.66
+	Casablanca:8133.31
+	Conakry:6477.79
+	Dakar:7167.58
+	Dar es Salaam:2702.99
+	Douala:4397.07
+Fez
+	Abidjan:3191.48
+	Accra:3204.56
+	Addis Ababa:5242.41
+	Alexandria:3265.59
+	Algiers:786.95
+	Antananarivo:8089.44
+	Bamako:2397.36
+	Brazzaville:4757.54
+	Cairo:3423.10
+	Cape Town:7935.91
+	Casablanca:248.10
+	Conakry:2868.59
+	Dakar:2490.22
+	Dar es Salaam:6497.30
+	Douala:3662.36
+	Durban:8042.36
+Freetown
+	Abidjan:1073.94
+	Accra:1472.08
+	Addis Ababa:5698.67
+	Alexandria:5120.09
+	Algiers:3545.28
+	Antananarivo:7315.74
+	Bamako:735.09
+	Brazzaville:3463.06
+	Cairo:5198.72
+	Cape Town:5772.80
+	Casablanca:2847.91
+	Conakry:124.81
+	Dakar:825.13
+	Dar es Salaam:6060.17
+	Douala:2579.82
+	Durban:6361.33
+	Fez:2962.88
+Giza
+	Abidjan:4594.09
+	Accra:4265.59
+	Addis Ababa:2457.09
+	Alexandria:180.99
+	Algiers:2707.31
+	Antananarivo:5709.50
+	Bamako:4462.95
+	Brazzaville:4167.74
+	Cairo:5.86
+	Cape Town:7228.91
+	Casablanca:3666.47
+	Conakry:5175.85
+	Dakar:5238.83
+	Dar es Salaam:4179.63
+	Douala:3663.62
+	Durban:6651.19
+	Fez:3420.98
+	Freetown:5193.84
+Harare
+	Abidjan:4627.13
+	Accra:4300.45
+	Addis Ababa:3099.58
+	Alexandria:5447.69
+	Algiers:6737.84
+	Antananarivo:1740.68
+	Bamako:5458.23
+	Brazzaville:2282.06
+	Cairo:5318.85
+	Cape Town:2185.12
+	Casablanca:7028.46
+	Conakry:5773.61
+	Dakar:6421.63
+	Dar es Salaam:1515.19
+	Douala:3372.96
+	Durban:1336.96
+	Fez:6909.57
+	Freetown:5669.75
+	Giza:5314.23
+Johannesburg
+	Abidjan:4923.26
+	Accra:4662.36
+	Addis Ababa:4079.34
+	Alexandria:6380.73
+	Algiers:7470.15
+	Antananarivo:2148.86
+	Bamako:5816.81
+	Brazzaville:2792.42
+	Cairo:6259.27
+	Cape Town:1263.45
+	Casablanca:7636.88
+	Conakry:6007.93
+	Dakar:6693.72
+	Dar es Salaam:2460.26
+	Douala:3900.02
+	Durban:497.20
+	Fez:7547.36
+	Freetown:5892.93
+	Giza:6254.46
+	Harare:979.95
+Kampala
+	Abidjan:4097.86
+	Accra:3682.61
+	Addis Ababa:1182.77
+	Alexandria:3442.79
+	Algiers:5059.90
+	Antananarivo:2688.25
+	Bamako:4673.73
+	Brazzaville:1984.69
+	Cairo:3306.85
+	Cape Town:4079.88
+	Casablanca:5582.81
+	Conakry:5217.44
+	Dakar:5717.07
+	Dar es Salaam:1086.18
+	Douala:2572.07
+	Durban:3355.84
+	Fez:5411.25
+	Freetown:5149.45
+	Giza:3302.43
+	Harare:2021.72
+	Johannesburg:2985.16
+Khartoum
+	Abidjan:4146.16
+	Accra:3740.72
+	Addis Ababa:988.90
+	Alexandria:1754.68
+	Algiers:3737.72
+	Antananarivo:4169.41
+	Bamako:4373.81
+	Brazzaville:2909.18
+	Cairo:1612.26
+	Cape Town:5700.16
+	Casablanca:4484.24
+	Conakry:5051.66
+	Dakar:5348.63
+	Dar es Salaam:2596.18
+	Douala:2805.03
+	Durban:5051.44
+	Fez:4268.83
+	Freetown:5026.15
+	Giza:1608.00
+	Harare:3715.45
+	Johannesburg:4668.10
+	Kampala:1697.00
+Kinshasa
+	Abidjan:2400.58
+	Accra:2043.47
+	Addis Ababa:2985.65
+	Alexandria:4240.26
+	Algiers:4739.89
+	Antananarivo:3849.82
+	Bamako:3191.45
+	Brazzaville:8.53
+	Cairo:4179.39
+	Cape Town:3303.13
+	Casablanca:4852.96
+	Conakry:3562.95
+	Dakar:4179.38
+	Dar es Salaam:2664.46
+	Douala:1120.53
+	Durban:3280.41
+	Fez:4766.06
+	Freetown:3469.33
+	Giza:4173.65
+	Harare:2274.29
+	Johannesburg:2783.92
+	Kampala:1983.41
+	Khartoum:2913.00
+Lagos
+	Abidjan:829.77
+	Accra:411.61
+	Addis Ababa:3896.09
+	Alexandria:3893.76
+	Algiers:3368.06
+	Antananarivo:5592.40
+	Bamako:1425.33
+	Brazzaville:1776.56
+	Cairo:3915.79
+	Cape Town:4755.93
+	Casablanca:3221.33
+	Conakry:1912.55
+	Dakar:2449.68
+	Dar es Salaam:4242.42
+	Douala:746.84
+	Durban:4995.01
+	Fez:3185.00
+	Freetown:1845.86
+	Giza:3910.13
+	Harare:4058.57
+	Johannesburg:4499.58
+	Kampala:3304.96
+	Khartoum:3331.33
+	Kinshasa:1784.30
+Luanda
+	Abidjan:2477.74
+	Accra:2185.46
+	Addis Ababa:3447.42
+	Alexandria:4790.07
+	Algiers:5177.17
+	Antananarivo:3855.31
+	Bamako:3346.05
+	Brazzaville:556.85
+	Cairo:4730.28
+	Cape Town:2836.49
+	Casablanca:5202.77
+	Conakry:3611.63
+	Dakar:4271.22
+	Dar es Salaam:2875.80
+	Douala:1484.67
+	Durban:2978.20
+	Fez:5137.76
+	Freetown:3505.81
+	Giza:4724.53
+	Harare:2166.39
+	Johannesburg:2484.18
+	Kampala:2369.10
+	Khartoum:3446.55
+	Kinshasa:551.08
+	Lagos:2018.52
+Lusaka
+	Abidjan:4238.34
+	Accra:3907.15
+	Addis Ababa:2948.85
+	Alexandria:5182.51
+	Algiers:6372.48
+	Antananarivo:2077.37
+	Bamako:5063.68
+	Brazzaville:1885.46
+	Cairo:5061.98
+	Cape Town:2281.14
+	Casablanca:6641.25
+	Conakry:5388.43
+	Dakar:6031.82
+	Dar es Salaam:1533.61
+	Douala:2976.30
+	Durban:1627.87
+	Fez:6526.11
+	Freetown:5286.01
+	Giza:5057.14
+	Harare:397.00
+	Johannesburg:1198.36
+	Kampala:1810.19
+	Khartoum:3476.51
+	Kinshasa:1877.72
+	Lagos:3662.02
+	Luanda:1789.67
+Maputo
+	Abidjan:5259.42
+	Accra:4976.01
+	Addis Ababa:3944.51
+	Alexandria:6358.00
+	Algiers:7624.38
+	Antananarivo:1719.66
+	Bamako:6136.83
+	Brazzaville:3034.89
+	Cairo:6225.74
+	Cape Town:1623.08
+	Casablanca:7860.52
+	Conakry:6366.69
+	Dakar:7043.94
+	Dar es Salaam:2244.58
+	Douala:4146.67
+	Durban:458.42
+	Fez:7755.87
+	Freetown:6254.36
+	Giza:6221.24
+	Harare:919.17
+	Johannesburg:450.41
+	Kampala:2920.18
+	Khartoum:4617.18
+	Kinshasa:3026.56
+	Lagos:4784.20
+	Luanda:2790.79
+	Lusaka:1254.33
+Nairobi
+	Abidjan:4592.28
+	Accra:4179.07
+	Addis Ababa:1165.14
+	Alexandria:3681.87
+	Algiers:5475.34
+	Antananarivo:2279.69
+	Bamako:5178.10
+	Brazzaville:2412.18
+	Cairo:3531.23
+	Cape Town:4099.70
+	Casablanca:6042.79
+	Conakry:5717.66
+	Dakar:6221.42
+	Dar es Salaam:671.15
+	Douala:3068.83
+	Durban:3232.78
+	Fez:5863.63
+	Freetown:5648.10
+	Giza:3527.39
+	Harare:1942.25
+	Johannesburg:2921.81
+	Kampala:504.64
+	Khartoum:1932.76
+	Kinshasa:2409.85
+	Lagos:3805.17
+	Luanda:2739.80
+	Lusaka:1827.98
+	Maputo:2779.57
+Omdurman
+	Abidjan:4137.12
+	Accra:3731.92
+	Addis Ababa:999.91
+	Alexandria:1747.93
+	Algiers:3726.58
+	Antananarivo:4178.47
+	Bamako:4363.35
+	Brazzaville:2906.22
+	Cairo:1606.16
+	Cape Town:5702.64
+	Casablanca:4472.65
+	Conakry:5041.47
+	Dakar:5337.76
+	Dar es Salaam:2604.35
+	Douala:2797.87
+	Durban:5056.46
+	Fez:4257.29
+	Freetown:5016.12
+	Giza:1601.87
+	Harare:3720.34
+	Johannesburg:4672.35
+	Kampala:1702.39
+	Khartoum:11.67
+	Kinshasa:2910.06
+	Lagos:3322.63
+	Luanda:3444.12
+	Lusaka:3480.39
+	Maputo:4622.55
+	Nairobi:1940.54
+Pretoria
+	Abidjan:4898.81
+	Accra:4634.09
+	Addis Ababa:4022.71
+	Alexandria:6325.52
+	Algiers:7424.62
+	Antananarivo:2118.44
+	Bamako:5789.90
+	Brazzaville:2753.53
+	Cairo:6203.74
+	Cape Town:1312.85
+	Casablanca:7597.82
+	Conakry:5987.98
+	Dakar:6672.25
+	Dar es Salaam:2405.76
+	Douala:3862.38
+	Durban:535.67
+	Fez:7506.73
+	Freetown:5873.49
+	Giza:6198.95
+	Harare:923.38
+	Johannesburg:56.70
+	Kampala:2928.68
+	Khartoum:4612.04
+	Kinshasa:2745.04
+	Lagos:4467.00
+	Luanda:2453.48
+	Lusaka:1143.41
+	Maputo:436.93
+	Nairobi:2865.34
+	Omdurman:4616.31
+Tripoli
+	Abidjan:3542.13
+	Accra:3339.67
+	Addis Ababa:3725.24
+	Alexandria:1586.06
+	Algiers:1018.36
+	Antananarivo:6813.96
+	Bamako:3111.95
+	Brazzaville:4134.04
+	Cairo:1739.09
+	Cape Town:7443.60
+	Casablanca:1930.39
+	Conakry:3786.14
+	Dakar:3690.88
+	Dar es Salaam:5201.10
+	Douala:3225.11
+	Durban:7219.87
+	Fez:1685.90
+	Freetown:3835.27
+	Giza:1736.79
+	Harare:5945.62
+	Johannesburg:6752.48
+	Kampala:4148.86
+	Khartoum:2736.56
+	Kinshasa:4142.04
+	Lagos:3106.84
+	Luanda:4636.90
+	Lusaka:5602.44
+	Maputo:6854.51
+	Nairobi:4534.49
+	Omdurman:2725.66
+	Pretoria:6703.06
