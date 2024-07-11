2c190dddf7803ce41ce6b0497f0bb76400ea0098
Anthony Starks <ajstarks@gmail.com>
2024-07-10 22:58:17 -0400

    between: improve error handling

--- cmd/between/between.go
+++ cmd/between/between.go
@@ -11,10 +11,15 @@ import (
 func main() {
 	const isofmt = "2006-01-02"
 	var begintime, endtime, unit string
-	flag.StringVar(&begintime, "begin", "2006-01-02", "begin time")
-	flag.StringVar(&endtime, "end", "2009-11-10", "end time")
-	flag.StringVar(&unit, "unit", "hour", "time unit (month, hour, minute, second)")
+	flag.StringVar(&begintime, "begin", "", "begin time")
+	flag.StringVar(&endtime, "end", "", "end time")
+	flag.StringVar(&unit, "unit", "hour", "time unit (month, hour, minute, second, ms)")
 	flag.Parse()
+
+	if begintime == "" || endtime == "" {
+		fmt.Fprintf(os.Stderr, "usage: between -begin YYYY-MM-DD -end YYYY-MM-DD -unit (hour, minute, or second\n")
+		os.Exit(1)
+	}
 
 	t0, err := time.Parse(isofmt, begintime)
 	if err != nil {
@@ -34,7 +39,11 @@ func main() {
 		between = t1.Sub(t0).Minutes()
 	case "sec", "second", "s":
 		between = t1.Sub(t0).Seconds()
+	case "ms":
+		between = float64(t1.Sub(t0).Milliseconds())
+	default:
+		fmt.Fprintf(os.Stderr, "%s is not a valid time unit (use one of hr, min, sec, ms)\n", unit)
+		os.Exit(4)
 	}
-
 	fmt.Printf("%s %s %.2f %s\n", begintime, endtime, between, unit)
 }
