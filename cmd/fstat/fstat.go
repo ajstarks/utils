4acf15465d89873136117ad55dc8e1abb7c9ad43
Anthony Starks <ajstarks@gmail.com>
2025-01-05 14:20:12 -0500

    fstat: add sorting options

--- cmd/fstat/fstat.go
+++ cmd/fstat/fstat.go
@@ -2,9 +2,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/fs"
 	"os"
+	"sort"
 )
+
+const (
+	dirfmt  = "%-15s %20s %15d\t%s\n"
+	timefmt = "2006-01-02 15:04:05"
+)
+
+type dirflags struct {
+	na, sa, nd, sd, older, newer, showdot bool
+}
 
 // status gets file status
 func status(filename string) (os.FileInfo, bool, error) {
@@ -15,9 +27,79 @@ func status(filename string) (os.FileInfo, bool, error) {
 	return f, f.IsDir(), err
 }
 
+// nameSort sorts directory entries by name (ascending)
+func nameSortAsc(files []fs.DirEntry) {
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].Name() < files[j].Name()
+	})
+}
+
+// sizeSort sort directory entries by file sizes (ascendiing)
+func sizeSortAsc(files []fs.DirEntry) {
+	sort.Slice(files, func(i, j int) bool {
+		fi, erri := files[i].Info()
+		fj, errj := files[j].Info()
+		if erri != nil || errj != nil {
+			return false
+		}
+		ni := fi.Size()
+		nj := fj.Size()
+		return ni < nj
+	})
+}
+
+// nameSort sorts directory entries by name (ascending)
+func nameSortDec(files []fs.DirEntry) {
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].Name() > files[j].Name()
+	})
+}
+
+// sizeSort sort directory entries by file sizes (ascendiing)
+func sizeSortDec(files []fs.DirEntry) {
+	sort.Slice(files, func(i, j int) bool {
+		fi, erri := files[i].Info()
+		fj, errj := files[j].Info()
+		if erri != nil || errj != nil {
+			return false
+		}
+		ni := fi.Size()
+		nj := fj.Size()
+		return ni > nj
+	})
+}
+
+// timeSortOlder sorts directory entries by time older to newer
+func timeSortOlder(files []fs.DirEntry) {
+	sort.Slice(files, func(i, j int) bool {
+		fi, erri := files[i].Info()
+		fj, errj := files[j].Info()
+		if erri != nil || errj != nil {
+			return false
+		}
+		ti := fi.ModTime()
+		tj := fj.ModTime()
+		return ti.Before(tj)
+	})
+}
+
+// timeSortNewer sorts directory entries by time newer to older
+func timeSortNewer(files []fs.DirEntry) {
+	sort.Slice(files, func(i, j int) bool {
+		fi, erri := files[i].Info()
+		fj, errj := files[j].Info()
+		if erri != nil || errj != nil {
+			return false
+		}
+		ti := fi.ModTime()
+		tj := fj.ModTime()
+		return ti.After(tj)
+	})
+}
+
 // dirstat shows directory infomation
-func dirstat(name string) {
-	f, err := os.Open(name)
+func dirstat(dirname string, sf dirflags) {
+	f, err := os.Open(dirname)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return
@@ -27,10 +109,35 @@ func dirstat(name string) {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		return
 	}
+	// set the sort option
+
+	if sf.na {
+		nameSortAsc(di)
+	}
+	if sf.nd {
+		nameSortDec(di)
+	}
+	if sf.sa {
+		sizeSortAsc(di)
+	}
+	if sf.sd {
+		sizeSortDec(di)
+	}
+	if sf.older {
+		timeSortOlder(di)
+	}
+	if sf.newer {
+		timeSortNewer(di)
+	}
+
+	// print the entries
 	for _, d := range di {
 		fi, err := d.Info()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
+			continue
+		}
+		if !sf.showdot && fi.Name()[0] == '.' {
 			continue
 		}
 		printstat(fi)
@@ -39,24 +146,42 @@ func dirstat(name string) {
 
 // printstat shows file status
 func printstat(f os.FileInfo) {
-	if f.Name()[0] != '.' {
-		fmt.Printf(
-			"%-15s %20s %15d\t%s\n",
-			f.Mode(),
-			f.ModTime().Format("2006-01-02 15:04:05"),
-			f.Size(),
-			f.Name())
-	}
+	fmt.Printf(dirfmt, f.Mode(), f.ModTime().Format(timefmt), f.Size(), f.Name())
 }
 
 func main() {
+	var df dirflags
+	flag.BoolVar(&df.showdot, "a", false, "show dot files")
+	flag.BoolVar(&df.na, "na", true, "sort by name ascending")
+	flag.BoolVar(&df.nd, "nd", false, "sort by name descending")
+	flag.BoolVar(&df.sa, "sa", false, "sort by size ascending")
+	flag.BoolVar(&df.sd, "sd", false, "sort by size descending")
+	flag.BoolVar(&df.older, "old", false, "sort by age oldest first")
+	flag.BoolVar(&df.newer, "new", false, "sort by age newest first")
+	flag.Parse()
+	args := flag.Args()
+
+	if df.na && df.nd {
+		fmt.Fprintln(os.Stderr, "pick one: name ascending or descending")
+		os.Exit(1)
+	}
+	if df.sa && df.sd {
+		fmt.Fprintln(os.Stderr, "pick one: size ascending or descinding")
+		os.Exit(1)
+	}
+	if df.older && df.newer {
+		fmt.Fprintln(os.Stderr, "pick one: old or new")
+		os.Exit(1)
+	}
+
 	// if no args, show the current directory
-	if len(os.Args) <= 1 {
-		dirstat(".")
+	if len(args) == 0 {
+		dirstat(".", df)
 		return
 	}
+
 	// for every argument, print directory or file info
-	for i, filename := range os.Args[1:] {
+	for i, filename := range args {
 		s, isdir, err := status(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -66,7 +191,7 @@ func main() {
 			if i > 0 {
 				fmt.Printf("%s:\n", filename)
 			}
-			dirstat(filename)
+			dirstat(filename, df)
 		} else {
 			printstat(s)
 		}
